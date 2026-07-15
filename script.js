9878df4eabf7433a8dcbaf00fc26ca9fca242b15
soyoon3270 <isoyun741@gmail.com>
2026-07-15 17:57:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("HeartGuard Loaded!");
+
+function startAssessment(){
+
+    alert("Assessment page coming next!");
+
+}
+function learnDCM(){
+
+    alert("Learn About DCM page coming soon!");
+
+}
+
+function researchPage(){
+
+    alert("Prevention & Lifestyle page coming soon!");
+
+}
