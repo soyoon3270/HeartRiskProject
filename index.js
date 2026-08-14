8e7c7a42a0ba427d44df8f65fe7ff4d28511b3de
soyoon3270 <isoyun741@gmail.com>
2026-08-13 23:12:12 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,31 +2,6 @@ function startAssessment() {
     window.location.href = "about.html";
 }
 
-
 function beginAssessment() {
     window.location.href = "assessment.html";
 }
-
-
-function setLanguage(lang){
-
-    localStorage.setItem("language", lang);
-
-    location.reload();
-
-}
-
-
-const lang = localStorage.getItem("language") || "en";
-
-
-document.getElementById("title").innerText =
-translations[lang].title;
-
-
-document.getElementById("subtitle").innerText =
-translations[lang].subtitle;
-
-
-document.getElementById("startButton").innerText =
-translations[lang].start;
